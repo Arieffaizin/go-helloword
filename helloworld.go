1c254fc05ea470b640da343836f06777daa96b95
Arief Faizin <arief.faizinz@gmail.com>
2018-04-11 03:49:47 +0700

    initial commit

---
+++ helloworld.go
@@ -0,0 +1,11 @@
+//Setiap file harus memiliki package, package "main" dieksekusi pertama kali
+package main
+
+//Untuk memasukkan package lain
+import "fmt"
+
+//Fungsi main pertama kali dieksekusi
+func main() {
+	//Println untuk memunculkan text
+	fmt.Println("hello world")
+}
